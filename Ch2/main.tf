5a09a53ca3afdd5f01657befe1280fc60d0fadc2
Priscilla Arinze <priscillaarinze@gmail.com>
2025-02-21 19:37:48 -0600

    -replace aws_instance resource with launch_configuration resource -add auto scaling group resource -add data sources

--- Ch2/main.tf
+++ Ch2/main.tf
@@ -2,10 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-04b4f1a9cf54c11d0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+### Resources
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-04b4f1a9cf54c11d0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                 #!/bin/bash
@@ -13,10 +14,41 @@ resource "aws_instance" "example" {
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-  user_data_replace_on_change = true
+  # Required when using a launch configuration with an auto scaling group
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags = {
-    Name = "terraform-example"
+# resource "aws_instance" "example" {
+#   ami                    = "ami-04b4f1a9cf54c11d0"
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.instance.id]
+
+#   user_data = <<-EOF
+#                 #!/bin/bash
+#                 echo "Hello, World" > index.html
+#                 nohup busybox httpd -f -p ${var.server_port} &
+#                 EOF
+
+#   user_data_replace_on_change = true
+
+#   tags = {
+#     Name = "terraform-example"
+#   }
+# }
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -32,6 +64,20 @@ resource "aws_security_group" "instance" {
 }
 
 
+### Data Sources
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+
+### Variables
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
