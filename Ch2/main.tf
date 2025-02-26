7a7e6b944d096b4a021ed93f65479c9f9ff61a59
Priscilla Arinze <priscillaarinze@gmail.com>
2025-02-25 19:51:44 -0600

    -replace launch_configuraton resource with launch_template
    -add security group for auto scaling group
    
    -add load balancer resource and other associated resources (target groups, listeners)
    
    -replace public_ip output variable with dns_name

--- Ch2/main.tf
+++ Ch2/main.tf
@@ -3,16 +3,34 @@ provider "aws" {
 }
 
 ### Resources
-resource "aws_launch_configuration" "example" {
+# Can't use this in conjunction with the autoscaling group; need to use launch template instead (as of October 2024)
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/launch_template
+# resource "aws_launch_configuration" "example" {
+#   image_id        = "ami-04b4f1a9cf54c11d0"
+#   instance_type   = "t2.micro"
+#   security_groups = [aws_security_group.instance.id]
+
+#   user_data = <<-EOF
+#                 #!/bin/bash
+#                 echo "Hello, World" > index.html
+#                 nohup busybox httpd -f -p ${var.server_port} &
+#                 EOF
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+resource "aws_launch_template" "example" {
   image_id        = "ami-04b4f1a9cf54c11d0"
   instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
+  user_data = base64encode(<<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
+  )
 
   # Required when using a launch configuration with an auto scaling group
   lifecycle {
@@ -20,6 +38,7 @@ resource "aws_launch_configuration" "example" {
   }
 }
 
+# Using launch template instead of aws_instance for autoscaling group
 # resource "aws_instance" "example" {
 #   ami                    = "ami-04b4f1a9cf54c11d0"
 #   instance_type          = "t2.micro"
@@ -39,8 +58,14 @@ resource "aws_launch_configuration" "example" {
 # }
 
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
+  launch_template {
+    id = aws_launch_template.example.id
+  }
+
   vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -63,6 +88,81 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = 200
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow all inbound traffic on the HTTP port
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic on HTTP port
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 ### Data Sources
 data "aws_vpc" "default" {
@@ -84,7 +184,7 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
