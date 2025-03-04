440ad49f4bfa9aaa273592cec365a8453c874e8a
Priscilla Arinze <priscillaarinze@gmail.com>
2025-03-03 18:55:45 -0600

    - use aws s3 for backend
    - add s3 bucket for storing state files
    
    - add dynamodb table for locking state files
    
    - set up terraform workspaces

--- Ch3/main.tf
+++ Ch3/main.tf
@@ -1,12 +1,97 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+terraform {
+  backend "s3" {
     bucket = "priscilla-terraform-up-and-running-state"
+    key    = "workspaces-example/terraform.tfstate"
+    region = "us-east-1"
 
-    # Prevent accidental deletion of this S3 bucket
-    lifecycle {
-        prevent_destroy = true
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+## Resources
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.s3_bucket_name
+
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Enable versioning on the S3 bucket so you can see the full version history of your state files
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption on the S3 bucket by default
+# Ensures state files and any secrets are encrypted at rest
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
+}
+
+# Add extra layer of protection by explicitly blocking all public access to S3 bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Use DynamoDB table (NoSQL, key-value store) to lock state file to prevent concurrent modifications
+# Supports consistent read operations and conditional write operations
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-04b4f1a9cf54c11d0"
+  instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
+}
+
+
+## Variables
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket to store Terraform state"
+  type        = string
+  default     = "priscilla-terraform-up-and-running-state"
+}
+
+variable "dynamodb_table_name" {
+  description = "The name of the DynamoDB table to use for state locking"
+  type        = string
+  default     = "terraform-up-and-running-locks"
+}
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket to store Terraform state"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table to use for state locking"
 }
