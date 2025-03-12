fa205151e383c577519073bf12bf535a65aacdde
Priscilla Arinze <priscillaarinze@gmail.com>
2025-03-11 19:18:08 -0500

    format spacing

--- Ch4 - Terraform Modules/stage/services/ch2-webserver-cluster/main.tf
+++ Ch4 - Terraform Modules/stage/services/ch2-webserver-cluster/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "webserver_cluster" {
     source = "../../../modules/services/ch2-webserver-cluster"
 
-    cluster_name = "webservers-stage"
+    cluster_name           = "webservers-stage"
     db_remote_state_bucket = "priscilla-terraform-up-and-running-state"
-    db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+    db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 }
