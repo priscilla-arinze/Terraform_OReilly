7bc401167c17242fa6a26ffeb7d6065b04e6f990
Priscilla Arinze <priscillaarinze@gmail.com>
2025-03-13 19:08:29 -0500

    -add recurring scheduled action with the aws_autoscaling_schedule resource
    -add module outputs

--- Ch4 - Terraform Modules/prod/services/ch2-webserver-cluster/main.tf
+++ Ch4 - Terraform Modules/prod/services/ch2-webserver-cluster/main.tf
@@ -13,6 +13,8 @@ terraform {
   }
 }
 
+
+### Modules
 module "webserver_cluster" {
     source = "../../../modules/services/ch2-webserver-cluster"
 
@@ -22,4 +24,26 @@ module "webserver_cluster" {
     instance_type          = "t2.micro" # can opt for a larger instance (m4.large [not on free tier]) for production
     min_size               = 2
     max_size               = 10
+}
+
+
+### Resources
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 10
+  recurrence            = "0 9 * * *" # 9am UTC every day
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size              = 2
+  max_size              = 10
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *" # 5pm UTC every day
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
 }
