734e8f4b011930a9889feeff77a538fbd2957c37
Priscilla Arinze <priscillaarinze@gmail.com>
2025-03-14 19:20:48 -0500

    extract ingress and egress inline blocks to each their own separate resource

--- Ch4 - Terraform Modules/modules/services/ch2-webserver-cluster/main.tf
+++ Ch4 - Terraform Modules/modules/services/ch2-webserver-cluster/main.tf
@@ -157,22 +157,28 @@ resource "aws_lb_target_group" "asg" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
+}
 
-  # Allow all inbound traffic on the HTTP port
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+# Allow all inbound traffic on the HTTP port
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+  
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
 
-  # Allow all outbound traffic on HTTP port
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
+# Allow all outbound traffic on HTTP port
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group_alb.id
+
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 ### Data Sources
